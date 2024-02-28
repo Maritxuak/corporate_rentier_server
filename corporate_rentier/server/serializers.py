a675815e626838f1b6c5659c8784c35219c2e212
Dmitry_Ubuntu <nikitin_dima2000dn@mail.ru>
2024-02-28 13:41:39 +0300

    [UPD] => upd serializer Service and Project

--- corporate_rentier/server/serializers.py
+++ corporate_rentier/server/serializers.py
@@ -2,17 +2,22 @@ from rest_framework import serializers
 from .models import *
 
 
+
+
+
+
+
+
 class DignitiesSerializer(serializers.ModelSerializer):
     class Meta:
         model = Dignities
         fields = ['id', 'name', 'dsc']
 
-
 class ServicesSerializer(serializers.ModelSerializer):
+    included_in_the_service = DignitiesSerializer(many=True, read_only=True)
     class Meta:
         model = Services
-        fields = '__all__'
-
+        fields = ['name', 'category', 'dsc', 'included_in_the_service']
 
 class DcsCatServicesSerializer(serializers.ModelSerializer):
     class Meta:
@@ -20,12 +25,7 @@ class DcsCatServicesSerializer(serializers.ModelSerializer):
         fields = ['id', 'category', 'name', 'dsc']
 
 
-class ProjectsSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Projects
-        fields = ['id', 'name', 'category_services', 'bw_preview_photo',
-                  'c_preview_photo', 'title_photo', 'dsc_project', 'dsc_task',
-                  'photo_task', 'dsc_realization', 'photo_realization', 'quotes']
+
 
 
 class QuotesSerializer(serializers.ModelSerializer):
@@ -33,6 +33,13 @@ class QuotesSerializer(serializers.ModelSerializer):
         model = Quotes
         fields = '__all__'
 
+class ProjectsSerializer(serializers.ModelSerializer):
+    quotes = QuotesSerializer(many=True, read_only=True)
+    class Meta:
+        model = Projects
+        fields = ['id', 'name', 'category_services', 'bw_preview_photo',
+                  'c_preview_photo', 'title_photo', 'dsc_project', 'dsc_task',
+                  'photo_task', 'dsc_realization', 'photo_realization', 'quotes']
 
 class FeedBackSerializer(serializers.ModelSerializer):
     class Meta:
